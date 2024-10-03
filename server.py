1f7c7b06f06bce66988580b9aafd5d117781cc1a
Francesco Ansaldi <144808117+ansald06@users.noreply.github.com>
2024-10-03 12:35:29 +0200

    server.py

--- server.py
+++ server.py
@@ -1,42 +1,53 @@
 import socket
 from threading import Thread
 
-MY_ADDRESS = ("192.168.1.112", 9000)
+MY_ADDRESS = ("0.0.0.0", 9000)  # Ascolta su tutte le interfacce
 BUFFER_SIZE = 4096
 
 class Receive(Thread):
-    def __init__(self, s):
+    def __init__(self, conn, address):
         super().__init__()
-        self.s = s
+        self.conn = conn
+        self.address = address
         self.running = True
-        self.sender_address= None
-        self.clients= {}
+
     def run(self):
         while self.running:
-            data, self.sender_address = self.s.recvfrom(BUFFER_SIZE)
-            if self.sender_address not in self.clients:
-                self.clients[self.sender_address]=self.sender_address
-            string = data.decode()
-            print(f"{self.sender_address}: {string}")
-            
+            try:
+                data = self.conn.recv(BUFFER_SIZE)
+                if data:
+                    string = data.decode()
+                    print(f"{self.address}: {string}")
+                else:
+                    break
+            except ConnectionResetError:
+                break
+
     def kill(self):
         self.running = False
+        self.conn.close()
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP socket
     s.bind(MY_ADDRESS)
-    
-    # Starting thread
-    receiver = Receive(s)
-    receiver.start()
+    s.listen(5)  # Ascolta fino a 5 connessioni simultanee
+
+    print("Server in ascolto...")
 
     while True:
-        # Send message
-        if receiver.sender_address != None:
-            for address in receiver.clients:
-                string = input("-> ")
-                binary_string = string.encode()
-                s.sendto(binary_string, receiver.clients[address])
+        conn, address = s.accept()  # Accetta una nuova connessione
+        print(f"Connessione accettata da {address}")
+
+        # Inizia un thread separato per gestire ciascun client
+        receiver = Receive(conn, address)
+        receiver.start()
+
+        while True:
+            string = input("-> ")
+            if string.lower() == "exit":
+                break
+            binary_string = string.encode()
+            conn.sendall(binary_string)
 
 if __name__ == "__main__":
     main()
